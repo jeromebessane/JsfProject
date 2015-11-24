9e46d4167491bbfcea3421d3b7206efe652dce68
unknown <WQRZ0513@EQ-EQ6101655.ad.francetelecom.fr>
2015-11-24 19:08:54 +0100

    create project and city class in ejb and home view in web

---
+++ JsfProject/JsfProject-ejb/src/main/java/org/ema/City.java
@@ -0,0 +1,41 @@
+package org.ema;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class City {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private String id;
+	private String name;
+	private String country;
+	
+	public City() {
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
+
---
+++ JsfProject/JsfProject-ejb/src/main/java/org/ema/CityView.java
@@ -0,0 +1,29 @@
+package org.ema;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Named
+@Stateless
+public class CityView {
+	
+	@PersistenceContext
+    private EntityManager manager;
+	
+	public void init(){
+		City city = new City();
+		city.setId("000");
+		city.setName("test");
+		city.setCountry("test");
+		
+		manager.persist(city);
+	}
+	
+	public List<City> getCity(){
+		return manager.createQuery("from City", City.class).getResultList();
+	}
+}
